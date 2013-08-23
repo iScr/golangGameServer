86e92747db64d51908a8099c0fc83e70b405e6b7
iscr <xxqscr@gmail.com>
2013-08-23 21:10:41 +0800

    nothing

--- serverSocket.go
+++ serverSocket.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"net"
@@ -14,7 +15,7 @@ const (
 )
 
 func main() {
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:798")
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", "127.0.0.1:7981")
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	checkError(err)
 
@@ -30,42 +31,43 @@ func main() {
 
 func handleClient(conn net.Conn) {
 	fmt.Println(conn.RemoteAddr())
-	// var buffer []byte
-	buffer := make([]byte, 1024)
+	// var readBuffer []byte
+	readBuffer := make([]byte, 1024)
+	// buffer := make([]byte, 1024)
 	isHeadLoaded := false
 	bodyLen := 0
-	cmd := 0
-	
+	// cmd := 0
+
 	readLen := 0
 
-	func reset() {
-		isHeadLoaded = false
-		bodyLen = 0
-		cmd = 0
-	}
-
 	for {
-		length, err := conn.Read(buffer)
+
+		length, err := conn.Read(readBuffer[readLen:])
 		readLen += length
 		checkError(err)
-		fmt.Println(readLen)
-		fmt.Println(bodyLen)
-		fmt.Println(cmd)
+		fmt.Println("readLen: ", readLen)
+		fmt.Println("bodyLen: ", bodyLen)
+		fmt.Println(string(readBuffer[0:readLen]))
+		// fmt.Println("cmd: ", cmd)
 		conn.Write([]byte("return"))
 
 		if !isHeadLoaded {
+			continue
 			if readLen > Head {
 				fmt.Println("收到数据")
-				lenSlice := buffer[0:4]
+				lenSlice := readBuffer[0:4]
 				bodyLen = int(binary.BigEndian.Uint32(lenSlice)) - Head
-				cmd = int(binary.BigEndian.Uint32(buffer[4:7]))
+				cmd = int(binary.BigEndian.Uint32(readBuffer[4:8]))
 
 				fmt.Println("包体长度 %d", bodyLen)
-				fmt.Println("cmd: %d", cmd)
+				// fmt.Println("cmd: %d", cmd)
 				isHeadLoaded = true
 			}
 		}
 		if isHeadLoaded {
+			if readLen >= bodyLen {
+
+			}
 
 		}
 
